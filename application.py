bf9b3931c2ff7236023f2e61418c1a9c08cb295f
Logon27 <logondz27@gmail.com>
2022-09-12 23:59:28 -0400

    Fixed Qpixmap downsizing
    Added minimum label size to allow the Qpixmap to downsize

--- application.py
+++ application.py
@@ -2,6 +2,12 @@ from PyQt5 import QtWidgets, QtCore
 from dialog import Ui_MainWindow
 import sys
 
+#The plan.
+#Get the cursor's current location. Figure out which one of the 748 pixels its location would map to.
+#Then based on the size of the image calculate the size of the brush I would need and draw a large "point"
+#This will allow me to mimic a smaller pixel density even though I have more pixels. 
+#Then I can convert the QPainter to an image and downscale it to 28x28 and pass it to the network.
+
 class ApplicationWindow(QtWidgets.QMainWindow):
 
     def __init__(self):
--- canvaswidget.py
+++ canvaswidget.py
@@ -1,7 +1,8 @@
 # Imports
+from inspect import _void
 from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtGui import QPainter, QBrush, QPen, QColor
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QPointF
 from PyQt5.QtWidgets import (QApplication, QLabel, QWidget)
 
 class CanvasWidget(QtWidgets.QWidget):
@@ -19,13 +20,15 @@ class MplCanvasWidget(QtWidgets.QLabel):
 
     def __init__(self):
         QWidget.__init__(self)   # Inherit from QWidget
+        #the Qpixmap will now downsize if you do not set a minimum size
+        self.setMinimumSize(28, 28);
         QWidget.setSizePolicy(self, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         QWidget.updateGeometry(self)
         self.initUI()
         # self.setMouseTracking(True)
 
     def initUI(self):
-        canvas = QtGui.QPixmap(300, 300)
+        canvas = QtGui.QPixmap(28, 28)
         #fill the canvas with black because it could be random uninitialized data
         canvas.fill(QColor(0,0,0))
         # canvas = canvas.scaled(64, 64, QtCore.Qt.KeepAspectRatio)
@@ -42,6 +45,8 @@ class MplCanvasWidget(QtWidgets.QLabel):
         painter = QtGui.QPainter(self.pixmap())
         painter.setPen(QPen(QColor(255, 255, 255), self.brushWidth))
         painter.drawLine(self.last_x, self.last_y, e.x(), e.y())
+        
+        #painter.drawPoint(10, 10)
         painter.end()
         self.update()
 
@@ -55,64 +60,6 @@ class MplCanvasWidget(QtWidgets.QLabel):
 
     def resizeEvent(self, event):
         pixmap = self.pixmap()
-        print(self.width())
         pixmap.fill(QColor(0,0,0))
-        pixmap=pixmap.scaled(self.width(), self.height(), Qt.IgnoreAspectRatio, Qt.SmoothTransformation)
+        pixmap=pixmap.scaled(self.width(), self.height(), Qt.KeepAspectRatio, Qt.SmoothTransformation)
         self.setPixmap(pixmap)
-
-# class CanvasWidget(QtWidgets.QWidget):
-#     def __init__(self, parent=None):
-#         #This class abstraction is purely for formatting and resizing purposes
-#         QtWidgets.QWidget.__init__(self, parent)   # Inherit from QWidget
-#         self.canvas = MplCanvasWidget()            # Create canvas object
-#         self.vbl = QtWidgets.QVBoxLayout()         # Set box for plotting
-#         self.vbl.addWidget(self.canvas)
-#         self.setLayout(self.vbl)
-
-# class MplCanvasWidget(QtWidgets.QWidget):
-
-#     brushWidth = 3
-
-#     def __init__(self):
-#         QWidget.__init__(self)   # Inherit from QWidget
-#         QWidget.setSizePolicy(self, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-#         QWidget.updateGeometry(self)
-#         self.initUI()
-#         # self.setMouseTracking(True)
-
-#     def initUI(self):
-#         self.label = QLabel(self)
-#         canvas = QtGui.QPixmap(300, 300)
-#         #fill the canvas with black because it could be random uninitialized data
-#         canvas.fill(QColor(0,0,0))
-#         # canvas = canvas.scaled(64, 64, QtCore.Qt.KeepAspectRatio)
-#         self.label.setPixmap(canvas)
-
-#         self.last_x, self.last_y = None, None
-
-#     def mouseMoveEvent(self, e):
-#         if self.last_x is None: # First event.
-#             self.last_x = e.x()
-#             self.last_y = e.y()
-#             return # Ignore the first time.
-
-#         painter = QtGui.QPainter(self.label.pixmap())
-#         painter.setPen(QPen(QColor(255, 255, 255), self.brushWidth))
-#         painter.drawLine(self.last_x, self.last_y, e.x(), e.y())
-#         painter.end()
-#         self.update()
-
-#         # Update the origin for next time.
-#         self.last_x = e.x()
-#         self.last_y = e.y()
-
-#     def mouseReleaseEvent(self, e):
-#         self.last_x = None
-#         self.last_y = None
-
-#     def resizeEvent(self, event):
-#         pixmap = self.label.pixmap()
-#         print(self.label.width())
-#         pixmap.fill(QColor(0,0,0))
-#         pixmap=pixmap.scaled(self.width(), self.height(), Qt.IgnoreAspectRatio, Qt.SmoothTransformation)
-#         self.label.setPixmap(pixmap)
