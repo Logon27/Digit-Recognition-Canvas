722eae6b34e287fdcbfd989fb03b7a151beb24f8
Logon27 <logondz27@gmail.com>
2022-09-15 20:43:52 -0400

    File I/O For Saving and Loading Networks

--- fileio.py
+++ fileio.py
@@ -1,10 +1,17 @@
-#Provide the ability to save and load neural network objects from files.
-
+import pickle
 from network import Network
 
+#Provides the ability to save and load neural network objects from files.
 
-def saveNetwork(filePath: str):
-    pass
+def saveNetwork(network: Network, filePath: str):
+    with open(filePath, "wb") as file:
+        print("Saving network to file {}...".format(filePath))
+        pickle.dump(network, file, pickle.HIGHEST_PROTOCOL)
+        print("Save complete.")
 
 def loadNetwork(filePath: str) -> Network:
-    pass
+    with open(filePath, "rb") as file:
+        print("Loading network from file {}...".format(filePath))
+        network = pickle.load(file)
+        print("Loading complete.")
+        return network
